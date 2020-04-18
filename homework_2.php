75aa7b2afdfd168124a3a7f4b129220a6f522914
anna-stefanova <astefanova267@gmail.com>
2020-04-18 17:39:11 +0300

    Homework 2 Task #7

---
+++ homework_2.php
@@ -0,0 +1,42 @@
+<?php
+# Задание № 7
+# Написать функцию, которая вычисляет текущее время и возвращает его в формате с правильными склонениями
+
+$hour = date('G');
+$minute = date('i');
+
+function formatHour($h) {
+    $format = null;
+    if ($h == 0 ||
+        ($h >= 5 && $h <= 20)) {
+        $format = 'часов';
+    }
+    if ($h % 10 == 1 && $h != 11) {
+        $format = 'час';
+    }
+    if ($h % 10 >= 2 && $h % 10 <= 4 && !($h >= 12 && $h <= 14)) {
+        $format = 'часа';
+    }
+    return $format;
+}
+
+function formatMinute($m) {
+    $format = null;
+    if ($m == 0 ||
+        $m % 10 == 0 ||
+        ($m >= 5 && $m <= 20) ||
+        ($m % 10 >= 5 && $m % 10 <= 9)) {
+        $format = 'минут';
+    }
+    if ($m % 10 >= 2 && $m % 10 <= 4 &&
+        !($m >= 12 && $m <= 14)) {
+        $format = 'минуты';
+    }
+    if ($m % 10 == 1 && $m != 11) {
+        $format = 'минута';
+    }
+    return $format;
+}
+
+echo $hour  . ' ' . formatHour($hour) . ' ' . $minute . ' ' . formatMinute($minute);
+
